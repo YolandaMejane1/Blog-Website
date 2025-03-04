07811eb9ff05a74737c836e577dda1f8bbd8971a
Yolanda Mejane <mysivuyile@gmail.com>
2025-03-04 17:23:22 +0200

    linked frontend to backend using axioms

---
+++ front-end/src/api/postApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios'; // axioms link the backend to the frontend
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5002/api/posts';
+// this is to get all posts
+export const getAllPosts = () => {
+    return axios.get(API_URL);
+  };
+// getting posts by their id
+  export const getPostById = (id) => {
+    return axios.get(`${API_URL}/${id}`);
+  };
+// function to create a new post
+  export const createPost = (postData) => {
+    return axios.post(API_URL, postData);
+  };
+  // function to update a post that already exists
+  export const updatePost = (id, updatedData) => {
+    return axios.put(`${API_URL}/${id}`, updatedData);
+  };
+// function to delete a post
+export const deletePost = (id) => {
+    return axios.delete(`${API_URL}/${id}`);
+  };
