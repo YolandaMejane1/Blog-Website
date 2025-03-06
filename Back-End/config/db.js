77be4246d72edaf097c5b746aee3b82fb7cd07f5
Yolanda Mejane <mysivuyile@gmail.com>
2025-03-06 11:05:11 +0200

    fixing post file error

--- Back-End/config/db.js
+++ Back-End/config/db.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";//loading the .env file
 const connectDB = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        
       });
       console.log("MongoDB Connected");
     } catch (error) {
