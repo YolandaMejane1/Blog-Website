4da69fb822ad87bd275ac74f2b8769cfb21edaca
Yolanda Mejane <mysivuyile@gmail.com>
2025-03-25 17:21:24 +0200

    deploy settings

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize('blog', 'root', process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: false, 
 });
