c36167e156220d1d98f8b49d563019a08480d6ad
Yolanda Mejane <mysivuyile@gmail.com>
2025-03-25 19:43:28 +0200

    debugging deplyment ports

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
 
 const app = express();
-const PORT = 5001;
+
+const PORT = process.env.PORT || 5001; 
 
 app.use(cors());
 
